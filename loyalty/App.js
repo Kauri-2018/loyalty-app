060cec7ff7fcbf973b46c953934747424b9ad64b
Zoe Lisk <zoe.lisk@gmail.com>
2018-05-10 10:45:32 +1200

    add file structure

--- loyalty/App.js
+++ loyalty/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import { StyleSheet, View } from 'react-native'
 
-import Home from './Home'
+import Home from './screens/Home'
 
 export default class App extends Component {
   render () {
