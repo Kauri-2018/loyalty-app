52367777a8adc70e7878f0445407c1388da1509b
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2018-05-10 15:56:13 +1200

    change login form to tcomb

--- loyalty/src/components/Login/Login.js
+++ loyalty/src/components/Login/Login.js
@@ -1,32 +1,33 @@
 import React, {Component} from 'react'
-import { View, TextInput, Text, Button } from 'react-native'
+import { View, TextInput, Text, Button, StyleSheet } from 'react-native'
+import t from 'tcomb-form-native'
+
+const Form = t.form.Form
+
+const User = t.struct({
+  username: t.String,
+  password: t.String
+})
 
 export default class Login extends Component {
-  render () {
+  render() {
     return (
-      <View >
-        <Text h1> Login to your account </Text>
-        <TextInput
-          style={{
-            height: 40,
-            borderColor: 'gray',
-            borderWidth: 1
-          }} >
-          Username:
-        </TextInput>
-        <TextInput
-          style={{
-            height: 40,
-            borderColor: 'gray',
-            borderWidth: 1
-          }} >
-          Password:
-        </TextInput>
-        <Button
-          // onPress={onPressLogin}
-          title='Login'
-          color='#841584' />
-      </View>
+      <View style={styles.container}>
+       <Form type={User} />
+       <Button
+          title="Log in"
+          onPress={this.handleSubmit}
+        />
+      </ View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
+  },
+})
