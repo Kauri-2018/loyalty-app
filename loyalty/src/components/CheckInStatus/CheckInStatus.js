c8abd5a53adea32247d2cb7d419c371d5709876d
Zoe Lisk <zoe.lisk@gmail.com>
2018-05-11 19:54:06 +1200

    checkinstatus comp renamed properly

--- loyalty/src/components/CheckInStatus/CheckInStatus.js
+++ loyalty/src/components/CheckInStatus/CheckInStatus.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
-export default class Login extends Component {
+export default class CheckInStatus extends Component {
   render () {
     return (
       <View style={styles.container}>
