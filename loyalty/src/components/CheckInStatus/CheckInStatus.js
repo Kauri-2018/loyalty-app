a478a801f93fd8e7d8ab617fdb88f54c8ee0cafd
Zoe Lisk <zoe.lisk@gmail.com>
2018-05-11 19:53:15 +1200

    add CheckInStatus component

---
+++ loyalty/src/components/CheckInStatus/CheckInStatus.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default class Login extends Component {
+  render () {
+    return (
+      <View style={styles.container}>
+        <Text p> Succcessfully checked in! </Text>
+        <Text h3> Welcome to the zoo, [username] </Text>
+      </ View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff'
+  }
+})
