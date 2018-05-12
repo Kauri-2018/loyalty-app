54a8748b9664dbf765070e06b1d2da0ec77deaa4
Frank <xuzy213@gmail.com>
2018-05-12 19:21:04 +1200

    add user photo on profile img component

--- src/components/ProfileImg/ProfileImg.js
+++ src/components/ProfileImg/ProfileImg.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react'
-import { StyleSheet, View, Image } from 'react-native'
+import React, {Component} from 'react'
+import {StyleSheet, View, Image} from 'react-native'
+import {connect} from 'react-redux'
 
-export default class ProfileImg extends Component {
-    render () {
-        return (
-          <View style={styles.Container}>
+class ProfileImg extends Component {
+  render () {
+    return (
+      <View style={styles.Container}>
+        {this.props.isAuth &&
           <Image
             style={{width: 250, height: 250, borderRadius: 125}}
-            source={{uri: 'https://1funny.com/wp-content/uploads/2011/11/fat-face-cat.jpg'}} >
-          </Image>
-        </View>
-        )
-    }
+            source={{uri: `${this.props.user.photo_url}`}} />
+        }
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -23,3 +25,11 @@ const styles = StyleSheet.create({
     borderRadius: 125
   }
 })
+
+const mapStateToProps = state => {
+  return {
+    isAuth: state.auth.isAuthenticated,
+    user: state.auth.user
+  }
+}
+export default connect(mapStateToProps)(ProfileImg)
