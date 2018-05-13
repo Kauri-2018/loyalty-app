be45d644555f435e48a4e0d206c4ba861e83dac2
Zoe Lisk <zoe.lisk@gmail.com>
2018-05-13 13:23:26 +1200

    logo component has image

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -7,7 +7,7 @@ export default class Logo extends Component {
       <View >
         <Image
           style={{width: 140, height: 100}}
-          source={{uri: 'http://via.placeholder.com/350x150'}} >
+          source={require('./LogoLoyalty1.png')} >
         </Image>
       </View>
     )
