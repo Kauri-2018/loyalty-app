49c428d25d06c5abe07dc74f380bd8d74bd9a894
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2018-05-15 11:39:49 +1200

    add styling to panel

--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -1,45 +1,49 @@
-import React, { Component, StyleSheet, Text, TouchableHighlight, Animated } from 'react-native'
+import React, { Component, Text, TouchableHighlight, Image } from 'react-native' // TODO bring in animated?
 import { Content } from 'native-base'
 
+import styles from './styles'
+
 class AccountPanel extends Component {
-    contructor(props) {
-        super(props)
+  contructor (props) {
+    super(props)
 
-        this.icons = {
-            'up': require('./up.png'),
-            'down': require('./down.png')
-        }
-        this.state = {
-            title: props.title,
-            expanded: true
-        }
+    this.icons = {
+      'up': require('./up.png'),
+      'down': require('./down.png')
     }
-    toggle() {
+    this.setState = {
+      title: props.title,
+      expanded: true
+    }
+  }
+  toggle () {
 
-    }
+  }
 
-    render() {
-        let icon = this.icons['down']
-        if(this.state.expanded){
-            icon = this.icons['up']
-        }
-        return (
-          <Content>
-            <Content>
-              <Text>{this.state.title}</Text>
-              <TouchableHighlight
-                onPress={this.toggle.bind(this)}>
-                <Image
-                source={icon}>
-                </Image>
-              </TouchableHighlight>
-            </Content>
-            <Content>
-              {this.props.children}
-            </Content>
-          </Content>
-        )
+  render () {
+    let icon = this.icons['down']
+    if (this.state.expanded) {
+      icon = this.icons['up']
     }
+    return (
+      <Content style={styles.container}>
+        <Content style={styles.titleContainer}>
+          <Text style={styles.title}>{this.state.title}</Text>
+          <TouchableHighlight
+            style={styles.button}
+            onPress={this.toggle.bind(this)}>
+            <Image
+              style={styles.buttonImage}
+              source={icon}>
+            </Image>
+          </TouchableHighlight>
+        </Content>
+        <Content style={styles.body}>
+          {this.props.children}
+        </Content>
+      </Content>
+    )
+  }
 }
 
 export default AccountPanel
